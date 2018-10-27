15c856ecbb7351118bd850266e979b2861433671
Ben Sunnquist <ben@Bens-MacBook-Pro.local>
2018-10-27 17:32:38 -0400

    Changed default weight

---
+++ esi_calc.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def esi_calc(xe, x, w=0.57):
+    """ The earth similar index.
+    Parameters
+    ----------
+    x  : int
+        Parameter value of the exoplanet
+    xe : int
+        Reference value (planet earth)
+    w  : int
+        Weighted value
+
+    Return
+    ------
+    esi : earth similarity index
+    """
+
+    esi = (1 - np.abs((xe - x)/(xe + x))**w
+
+    return(esi)
